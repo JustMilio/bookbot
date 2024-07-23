ea5e84c5daa47ab6adcf0308b89c854ba187b004
ThePrimeagen <edaniels29@gmail.com>
2024-07-23 22:41:48 +0200

    Could've done better

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 def main():
-    # with open("books/frankenstein.txt") as f:
-    #     file_contents = f.read()
-    #     print(file_contents)
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    word_count(text)
-    # print(text)
+    total_words = word_count(text)
+    lowered_text = lower_case(text)
+    lowered_dict = letter_count(lowered_text)
+    sorted_dict_list = list_of_dicts(lowered_dict)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{total_words} words were found in the document")
+    print()
+
+    for item in sorted_dict_list:
+        print(f"The '{item['char']}' character was found {item['num']} times")
+    
+    print("--- End report ---")
+
+def list_of_dicts(dict):
+    sorted_list = []
+    for ch in dict:
+        sorted_list.append({"char": ch, "num": dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_dict)
+    return sorted_list
 
 def get_book_text(path):
     with open(path) as f:
@@ -13,8 +28,20 @@ def get_book_text(path):
 
 def word_count(s):
     words = len(s.split())
-    print(words)
     return words
 
+def lower_case(words):
+    lowered = words.lower()
+    return lowered
+
+def letter_count(s):
+    d = {}
+    for i in s:
+        if i.isalpha():
+            d[i] = d.get(i,0) + 1 
+    return d
+
+def sort_dict(dict):
+    return dict["num"]
 
 main()
